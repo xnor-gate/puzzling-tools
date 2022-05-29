d6eca450015f67520248eab178f630e6d57e55c1
xnor-golf <programmeralex@gmail.com>
2022-05-29 17:04:10 -0400

    access to nutrimatic hehehe

--- my_javascript.js
+++ my_javascript.js
@@ -1,6 +1,19 @@
 function makeText() {
     console.log("in the function");
     // alert("Hello Javatpoint");  
-    document.getElementById("BodyId").innerText = "The button got pushed yay!";
-    document.querySelector(".button").innerText = "NEW"
+    document.getElementById("BodyId").innerText = "Wait it did! Wow!";
+    document.querySelector(".button").innerText = "NEW";
+
+    //https://www.quinapalus.com/cgi-bin/qat?pat=cat&ent=Search&dict=0
+
+
+    let request = new XMLHttpRequest();
+    request.open("GET", 'https://cors-anywhere.herokuapp.com/https://nutrimatic.org/?q=OURCAT&go=Go', true);
+    request.onload = () => {console.log(request.responseText)}
+    request.send();
+
+    // const Http = new XMLHttpRequest();
+    // const url='https://www.quinapalus.com/cgi-bin/qat?pat=OURCAT&ent=Search&dict=0';
+    // Http.open("GET", url);
+    // Http.send();
   }
