22ade12792945a7697c59899bccb48f0935564e7
xnor-golf <programmeralex@gmail.com>
2022-05-29 16:09:39 -0400

    working button things

--- my_javascript.js
+++ my_javascript.js
@@ -1,3 +1,6 @@
-function square(number) {
-    return number * number;
+function makeText() {
+    console.log("in the function");
+    // alert("Hello Javatpoint");  
+    document.getElementById("BodyId").innerText = "The button got pushed yay!";
+    document.querySelector(".button").innerText = "NEW"
   }
